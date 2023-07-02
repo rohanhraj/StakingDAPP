063fdfdb497e4f09739e8577aec0232296bae12c
Rohan H Raj <96742536+rohanhraj@users.noreply.github.com>
2023-07-02 21:27:48 +0530

    files commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# StakingDAPP
-staking dapp is where users can stake tokens and receive rewards, Rewards are generated dynamically based on amount of time tokens are staked
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ components/StakeForm.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { useWeb3Contract } from 'react-moralis';
+import StakingAbi from '../constants/Staking.json';
+import TokenAbi from '../constants/RewardToken.json';
+import { Form } from 'web3uikit';
+import { ethers } from 'ethers';
+
+function StakeForm() {
+  const stakingAddress = "0x06cDF737B30C2c56c80346a057C2B04Bc5050B72"; //replace this with the address where you have deployed your staking Smart Contract
+  const tesTokenAddress = "0x772d9B673A6b14F8d842614EA5d56653E79a8796"; //replace this with the address where you have deployed your Reward Token Smart Contract
+
+  const { runContractFunction } = useWeb3Contract();
+
+  let approveOptions = {
+    abi: TokenAbi.abi,
+    contractAddress: tesTokenAddress,
+    functionName: 'approve'
+  };
+
+  let stakeOptions = {
+    abi: StakingAbi.abi,
+    contractAddress: stakingAddress,
+    functionName: 'stake'
+  };
+
+  async function handleStakeSubmit(data) {
+    const amountToApprove = data.data[0].inputResult;
+    approveOptions.params = {
+      amount: ethers.utils.parseEther(amountToApprove, 'ether'),
+      spender: stakingAddress
+    };
+
+    const tx = await runContractFunction({
+      params: approveOptions,
+      onError: (error) => console.log(error),
+      onSuccess: () => {
+        handleApproveSuccess(approveOptions.params.amount);
+      }
+    });
+  }
+
+  async function handleApproveSuccess(amountToStakeFormatted) {
+    stakeOptions.params = {
+      amount: amountToStakeFormatted
+    };
+
+    const tx = await runContractFunction({
+      params: stakeOptions,
+      onError: (error) => console.log(error)
+    });
+
+    await tx.wait();
+    console.log('Stake transaction complete');
+  }
+
+  return (
+    <div className='text-black'>
+      <Form
+        onSubmit={handleStakeSubmit}
+        data={[
+          {
+            inputWidth: '50%',
+            name: 'Amount to stake ',
+            type: 'number',
+            value: '',
+            key: 'amountToStake'
+          }
+        ]}
+        title="Stake Now!"
+      ></Form>
+    </div>
+  );
+}
+
+export default StakeForm;
